459fd69fe192aec07ac36bce8f887f72071f4959
Nasir Khan <nak@krank.com>
2019-11-07 18:26:07 +0500

    Phone Contacts and app icon added

--- My Salah Record/Custom Cells/TopPerformersCell.swift
+++ My Salah Record/Custom Cells/TopPerformersCell.swift
@@ -13,10 +13,17 @@ class TopPerformersCell: UITableViewCell {
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var subTile: UILabel!
+    @IBOutlet weak var btnImg: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.btnImg.setTitleColor(.white, for: .normal)
+        self.btnImg.layer.cornerRadius = self.btnImg.frame.size.height/2
+        self.btnImg.clipsToBounds = true
+        self.btnImg.layer.borderWidth = 1
+        self.btnImg.layer.borderColor = UIColor.gray.cgColor
         
         self.img.layer.cornerRadius = self.img.frame.size.height/2
         self.img.clipsToBounds = true
--- My Salah Record/TabbarVCs/TopPerformersVC.swift
+++ My Salah Record/TabbarVCs/TopPerformersVC.swift
@@ -7,22 +7,62 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
 class TopPerformersVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var table: UITableView!
+    let contactStore = CNContactStore()
+    var contacts = [CNContact]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         table.tableFooterView = UIView()
+        
+        let keys = [
+            CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
+            CNContactPhoneNumbersKey,
+            CNContactEmailAddressesKey,
+            CNContactImageDataKey
+            ] as [Any]
+        let request = CNContactFetchRequest(keysToFetch: keys as! [CNKeyDescriptor])
+        do {
+            try contactStore.enumerateContacts(with: request){
+                (contact, stop) in
+                // Array containing all unified contacts from everywhere
+                self.contacts.append(contact)
+                for phoneNumber in contact.phoneNumbers {
+                    if let number = phoneNumber.value as? CNPhoneNumber, let label = phoneNumber.label {
+                        let localizedLabel = CNLabeledValue<CNPhoneNumber>.localizedString(forLabel: label)
+                        print("\(contact.givenName) \(contact.familyName) tel:\(localizedLabel) -- \(number.stringValue), email: \(contact.emailAddresses)")
+                    }
+                }
+            }
+            self.table.reloadData()
+        } catch {
+            print("unable to fetch contacts")
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TopPerformersCell
+        let contact = contacts[indexPath.row]
+        
+        cell.selectionStyle = .none
+        cell.title.text = "\(contact.givenName) \(contact.familyName)"
+        cell.btnImg.isEnabled = false
+        
+        if let image = contact.imageData {
+            cell.btnImg.setImage(UIImage(data: image), for: .normal)
+        } else {
+            setDefaultImage(name: cell.title.text ?? "", button: cell.btnImg)
+        }
         
         return cell
     }
@@ -30,5 +70,22 @@ class TopPerformersVC: UIViewController, UITableViewDelegate, UITableViewDataSou
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard  let vc = self.navigationController?.storyboard?.instantiateViewController(withIdentifier: "ProfileVC") else { return }
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func setDefaultImage(name: String, button: UIButton) {
+        let initials = name.components(separatedBy: " ").reduce("") { ($0 == "" ? "" : "\($0.first!)") + "\($1.first!)" }
+        button.setTitle(initials, for: .normal)
+        button.backgroundColor = pickColor(alphabet: name[name.startIndex])
+    }
+    
+    func pickColor(alphabet: Character) -> UIColor {
+        let alphabetColors = [0x5A8770, 0xB2B7BB, 0x6FA9AB, 0xF5AF29, 0x0088B9, 0xF18636, 0xD93A37, 0xA6B12E, 0x5C9BBC, 0xF5888D, 0x9A89B5, 0x407887, 0x9A89B5, 0x5A8770, 0xD33F33, 0xA2B01F, 0xF0B126, 0x0087BF, 0xF18636, 0x0087BF, 0xB2B7BB, 0x72ACAE, 0x9C8AB4, 0x5A8770, 0xEEB424, 0x407887]
+        let str = String(alphabet).unicodeScalars
+        let unicode = Int(str[str.startIndex].value)
+        if 65...90 ~= unicode {
+            let hex = alphabetColors[unicode - 65]
+            return UIColor(red: CGFloat(Double((hex >> 16) & 0xFF)) / 255.0, green: CGFloat(Double((hex >> 8) & 0xFF)) / 255.0, blue: CGFloat(Double((hex >> 0) & 0xFF)) / 255.0, alpha: 1.0)
+        }
+        return UIColor.black
     }
 }
